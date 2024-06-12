70161f175989b66a02fa9d1b613a384946b241e1
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-13 05:34:36 +0800

    bagshaas asuuh

--- repeat.js
+++ repeat.js
@@ -1,4 +1,28 @@
-for (let i = 0; i <= 10; i++) {
-  naturalToo += 1;
+//daalgavar1
+let too = "";
+for (let i = 1; i <= 10; i++) {
+  too += "Натурал тоо " + i + "<br/>";
 }
-console.log(i);
+console.log(too);
+
+for (let H = 1; H <= 10; H++) {
+  console.log("Натурал тоо бол : ", H);
+}
+
+//daalgavar2
+for (let Y = 1; Y <= 10; Y + 2) {
+  console.log("Сондгой тоо : ", Y);
+}
+
+//daalgavar3
+
+//daalgavar4
+let Too = 0;
+let sum = 0;
+for (let X = 0; X <= 100; X++) {
+  Too += 1;
+  sum = sum + Too;
+}
+console.log("1 - 100 хүртэлх тооны нийлбэр", sum);
+
+//daalgavar5
